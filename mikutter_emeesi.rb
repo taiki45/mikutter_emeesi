4677b77bd6b8129bcbcdf3a9f17db2cd9b6f2fe6
Taiki ONO <taiks.4559@gmail.com>
2013-03-21 21:19:53 +0900

    add reply

--- mikutter_emeesi.rb
+++ mikutter_emeesi.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-require "socket"
+require 'socket'
+require 'json'
 
 Plugin.create :mikutter_emeesi do
   config_path = File.expand_path('../config', __FILE__)
@@ -12,6 +13,27 @@ Plugin.create :mikutter_emeesi do
         Service.primary.update(message: s.read)
         s.close
       end
+    end
+  end
+
+  command(
+    :mac_reply,
+    name: 'reply in other window',
+    condition: -> _ { true },
+    visible: true,
+    role: :timeline
+  ) do |opt|
+    Thread.new do
+      msg = opt.messages.first
+      socket = TCPSocket.new('localhost', port + 1)
+      socket.write(JSON.generate({
+        'screen_name' => msg.user.idname,
+        'status_id' => msg.id,
+        'message' => msg[:message]
+      }))
+      res = socket.gets
+      Service.primary.update(message: res, replyto: msg)
+      socket.close
     end
   end
 end
